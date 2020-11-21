4647d992af85327824d7d663d43e175f8b5c416d
Kamil Zok <zokam1411@gmail.com>
2020-11-21 17:12:56 +0000

    Add figures array with lego figures images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-console.log("Hello Kamil")
+const figures = [
+    "lego1.png",
+    "lego1.png",
+    "lego2.png",
+    "lego2.png",
+    "lego3.png",
+    "lego3.png",
+    "lego4.png",
+    "lego4.png",
+    "lego5.png",
+    "lego5.png",
+    "lego6.png",
+    "lego6.png",
+    "lego7.png",
+    "lego7.png",
+    "lego8.png",
+    "lego8.png"
+]
