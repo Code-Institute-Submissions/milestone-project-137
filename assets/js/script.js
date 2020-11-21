b34f994e5e4ed65c4e7f3742790ff537d6731bb8
Kamil Zok <zokam1411@gmail.com>
2020-11-21 19:40:05 +0000

    Add global variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,17 @@ const figures = [
     "lego8.png",
     "lego8.png"
 ]
+
+// Global variables
+const easy_button = document.getElementById("easy");
+const medium_button = document.getElementById("medium");
+const hard_button = document.getElementById("hard");
+
+const mainMenuSection = document.getElementById("main-menu-section");
+
+const gameArenaSection = document.getElementById("game-arena-section");
+const board_div = document.getElementById("board");
+let points_p = document.getElementById("points");
+let level_p = document.getElementById("level");
+let time_p = document.getElementById("time");
+let counter_p = document.getElementById("counter");
