0af9e7f68b75b3848cbec5c78117a0d8404033f7
Kamil Zok <zokam1411@gmail.com>
2020-12-08 17:58:16 +0000

    Beautify figures array to vertical

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
 // Array with lego figures
-const figures = ["lego1.png", "lego1.png", "lego2.png", "lego2.png", "lego3.png", "lego3.png", "lego4.png", "lego4.png", "lego5.png", "lego5.png", "lego6.png", "lego6.png", "lego7.png", "lego7.png", "lego8.png", "lego8.png"];
+const figures = [
+    "lego1.png",
+    "lego1.png",
+    "lego2.png",
+    "lego2.png",
+    "lego3.png",
+    "lego3.png",
+    "lego4.png",
+    "lego4.png",
+    "lego5.png",
+    "lego5.png",
+    "lego6.png",
+    "lego6.png",
+    "lego7.png",
+    "lego7.png",
+    "lego8.png",
+    "lego8.png"
+];
 // ----------------------- Global variables
 // Main menu section
 const mainMenuSection = document.getElementById("main-menu-section");
